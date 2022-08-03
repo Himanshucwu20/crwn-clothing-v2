e62b2d00995992f6bde729db1ed4544d919a6cfa
Himanshucwu20 <77524402+Himanshucwu20@users.noreply.github.com>
2022-08-04 01:07:28 +0530

    Update firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCO6x5_4xdMj3ockWd31RGG8xtegVAeM9A",
-    authDomain: "hsr-clothing-db.firebaseapp.com",
-    projectId: "hsr-clothing-db",
-    storageBucket: "hsr-clothing-db.appspot.com",
-    messagingSenderId: "825667945936",
-    appId: "1:825667945936:web:c12aaf7cf2ef373315a5d5"
+  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
+  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
+  projectId: 'crwn-clothing-db-98d4d',
+  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
+  messagingSenderId: '626766232035',
+  appId: '1:626766232035:web:506621582dab103a4d08d6',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
