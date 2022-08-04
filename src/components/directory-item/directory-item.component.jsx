81061ba1d35277098adebbd9d8bef001c31de513
Himanshucwu20 <himanshurmjpru@gmail.com>
2022-08-04 14:55:34 +0530

    before dep

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,5 +1,5 @@
 import './directory-item.styles.scss';
-
+import { Link } from 'react-router-dom';
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
@@ -10,10 +10,16 @@ const DirectoryItem = ({ category }) => {
           backgroundImage: `url(${imageUrl})`,
         }}
       />
+      
       <div className='body'>
-        <h2>{title}</h2>
+      <Link className='bodyl' to={`shop/${title}`}>
+        <h2>
+          {title}
+        </h2>
         <p>Shop Now</p>
+      </Link>
       </div>
+      
     </div>
   );
 };
