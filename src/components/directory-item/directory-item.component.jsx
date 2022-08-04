a344a9aea0624014f56c5571b8055be74e30150a
Himanshucwu20 <77524402+Himanshucwu20@users.noreply.github.com>
2022-08-04 14:46:55 +0530

    Update directory-item.component.jsx

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -11,9 +11,9 @@ const DirectoryItem = ({ category }) => {
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <Link className='bodyl' to={`shop/${title}`}>
-        <h2>{title}</h2>
-        <p>Shop Now</p>
-      </Link>
+           <h2>{title}</h2>
+           <p>Shop Now</p>
+        </Link>
       </Body>
     </DirectoryItemContainer>
   );
